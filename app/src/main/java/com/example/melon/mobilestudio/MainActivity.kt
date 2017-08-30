e6291fd41e032b60788fc88fa6a23d086aecb349
Jimingyu <melongl11@naver.com>
2017-08-30 14:53:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/melon/mobilestudio/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.melon.mobilestudio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView.text = " Hello Kotlin"
+
+    }
+}
