1fb881436911e103cdf1fa9baf0562e992e40a7f
refuse1993 <refuse1993@gmail.com>
2017-09-13 20:14:03 +0900

    주문확인 팝업

--- app/src/main/java/com/example/melon/mobilestudio/OrderActivity.kt
+++ app/src/main/java/com/example/melon/mobilestudio/OrderActivity.kt
@@ -3,6 +3,7 @@ package com.example.melon.mobilestudio
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.widget.ArrayAdapter
 import android.widget.CursorAdapter
 import kotlinx.android.synthetic.main.activity_order2.*
@@ -14,11 +15,20 @@ class OrderActivity : AppCompatActivity() {
         setContentView(R.layout.activity_order2)
 
         finalorder.setOnClickListener {
-            val intent = Intent(this, OrderFinishActivity::class.java)
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setMessage("주문하시겠습니까?")
+            builder.setPositiveButton("예"){dialog, whichButton ->
+                val intent = Intent(this,OrderFinishActivity::class.java)
+                startActivity(intent)
+            }
+            builder.setNegativeButton("아니오"){dialog,whichButton ->
+                dialog.cancel()
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+
             intent.putExtra("address",et_useraddress.text.toString())
             intent.putExtra("require",et_require.text.toString())
-            startActivity(intent)
         }
-
     }
 }
