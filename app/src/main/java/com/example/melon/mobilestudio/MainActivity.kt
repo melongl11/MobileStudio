a419e0066bc57ca7b7c97ad2c0a626736a0aafa9
refuse1993 <refuse1993@gmail.com>
2017-09-13 21:15:51 +0900

    app icon,name,actionbar color

--- app/src/main/java/com/example/melon/mobilestudio/MainActivity.kt
+++ app/src/main/java/com/example/melon/mobilestudio/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.melon.mobilestudio
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.Menu
 import android.view.View
+import android.widget.ActionMenuView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
