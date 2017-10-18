24a745d6a36c814ce11cfaa1bc73c18fd6a7e735
refuse1993 <refuse1993@gmail.com>
2017-10-18 21:24:53 +0900

    방문시간용 리스트 만드는중

--- mobilestudio_laundry/src/main/java/com/example/mobilestudio_laundry/ManagementActivity.kt
+++ mobilestudio_laundry/src/main/java/com/example/mobilestudio_laundry/ManagementActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.support.annotation.IntegerRes
 import android.support.v4.app.ActivityCompat
 import android.view.View
@@ -12,12 +13,12 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.firebase.ui.storage.images.FirebaseImageLoader
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_management.*
 import kotlinx.android.synthetic.main.activity_modify.*
+import java.util.ArrayList
 import java.util.HashMap
 
 class ManagementActivity : AppCompatActivity() {
@@ -25,6 +26,9 @@ class ManagementActivity : AppCompatActivity() {
     private var mAuth: FirebaseAuth? = null
     private var mAuthListener: FirebaseAuth.AuthStateListener? = null
     private var userID:String = ""
+
+    private var datas = ArrayList<Visittime>()
+    lateinit var adapter : VisittimeListActivity
 /*
     private var mStorageRef : StorageReference = FirebaseStorage.getInstance().getReference()
 
@@ -47,7 +51,15 @@ class ManagementActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         mAuth!!.addAuthStateListener(mAuthListener!!)
+     // Handler().postDelayed({
+        val dbR = FirebaseDatabase.getInstance().getReference("/laundry/$userID/info/time")
+        dbR.addValueEventListener(postListener)
+
+        adapter = VisittimeListActivity(datas, this)
+        lv_visittime.setAdapter(adapter)
+     //      },1000)
     }
+
     override fun onStop() {
         super.onStop()
         if(mAuthListener != null) {
@@ -63,7 +75,7 @@ class ManagementActivity : AppCompatActivity() {
         } else {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100)
         }
-
+        
         supportActionBar?.run {
             setTitle("가게관리")
         }
@@ -132,12 +144,26 @@ class ManagementActivity : AppCompatActivity() {
     fun newtime(time: Int,time2 : Int) {
 
         val childUpdate = HashMap<String, Any>()
-
         val result: HashMap<String, Any> = HashMap<String, Any>()
         result.put("hourOfDay", time)
         result.put("minute",time2)
         childUpdate.put("/laundry/"+userID+"/info/time", result)
         mDatabase.updateChildren(childUpdate)
+    }
+
+    private val postListener = object : ValueEventListener {
+        override fun onDataChange(datasnapshot: DataSnapshot) {
+            datas.clear()
+            for(snapshot in datasnapshot.getChildren()) {
+                val visit = snapshot.getValue(Visittime::class.java)
+                datas.add(visit!!)
+            }
+            adapter.notifyDataSetChanged()
+        }
+
+        override fun onCancelled(p0: DatabaseError?) {
+
+        }
     }
 
 }
