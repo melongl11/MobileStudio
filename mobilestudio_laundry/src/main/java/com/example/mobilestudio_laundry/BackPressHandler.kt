ef9a672f23f43d7f521c56f667ea62799d6018b8
refuse1993 <refuse1993@gmail.com>
2017-11-09 13:25:21 +0900

    세탁소 backpress, 메인 sidebar 구현

---
+++ mobilestudio_laundry/src/main/java/com/example/mobilestudio_laundry/BackPressHandler.kt
@@ -0,0 +1,29 @@
+package com.example.mobilestudio_laundry
+
+import android.app.Activity
+import android.widget.Toast
+
+/**
+ * Created by melon on 2017-09-29.
+ */
+class BackPressHandler {
+    private var backKeyPressedTime :Long = 0
+    private var activity: Activity? = null
+
+    constructor(context: Activity) {
+        this.activity = context
+    }
+
+    fun onBackPressedEnd() {
+        if(System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis()
+            Toast.makeText(activity!!, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show()
+            return;
+        }
+        if(System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            for( i in arrayListforActivity)
+                i.finish()
+            activity!!.finish()
+        }
+    }
+}
--- mobilestudio_laundry/src/main/java/com/example/mobilestudio_laundry/MainActivity.kt
+++ mobilestudio_laundry/src/main/java/com/example/mobilestudio_laundry/MainActivity.kt
@@ -1,37 +1,55 @@
 package com.example.mobilestudio_laundry
 
+import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
+val arrayListforActivity = ArrayList<Activity>()
 class MainActivity : AppCompatActivity() {
 
+    private var backPressedHandler = BackPressHandler(this)
     var mAuth : FirebaseAuth? = null
+    private lateinit var drawerToggle:ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance()
 
+        val drawerLayout: DrawerLayout = findViewById(R.id.Drawerlayout)
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.app_name,R.string.app_name)
+        drawerLayout.addDrawerListener(drawerToggle)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         bt_orderedlist.setOnClickListener{
             val intent = Intent(this,OrderedActivity::class.java)
+            arrayListforActivity.add(this)
             startActivity(intent)
         }
 
         bt_acceptedlist.setOnClickListener{
             val intent = Intent(this,AcceptedList::class.java)
+            arrayListforActivity.add(this)
             startActivity(intent)
         }
         bt_management.setOnClickListener{
             val intent = Intent(this,ManagementActivity::class.java)
+            arrayListforActivity.add(this)
             startActivity(intent)
         }
         bt_signout.setOnClickListener{
+            arrayListforActivity.add(this)
             signout()
         }
     }
@@ -48,4 +66,25 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    override fun onBackPressed() {
+        backPressedHandler.onBackPressedEnd()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (drawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
 }
